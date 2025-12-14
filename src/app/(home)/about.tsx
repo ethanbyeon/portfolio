8ec62e93fabf7baffc0955beaa5e206aff38fc8d
Ethan Byeon <ethanbyeon+github@gmail.com>
2025-12-13 18:28:53 -0800

    chore: condense About description & increase About text visibility on light mode

--- src/app/(home)/about.tsx
+++ src/app/(home)/about.tsx
@@ -14,8 +14,10 @@ export const About = () => (
   >
     {/* Hero */}
     <div className="space-y-3 leading-relaxed">
-      <h1 className="text-4xl font-bold text-balance text-neutral-400 dark:text-primary">
-        <span className="text-primary dark:text-neutral-400">Hi, I&apos;m Ethan.</span>
+      <h1 className="text-4xl font-bold text-balance text-neutral-500 dark:text-primary">
+        <span className="text-primary dark:text-neutral-400">
+          Hi, I&apos;m Ethan.
+        </span>
         <br />
         Software engineer from Los Angeles,
         <br />
@@ -26,17 +28,15 @@ export const About = () => (
     {/* Body */}
     <div className="space-y-2 leading-relaxed font-medium text-neutral-500 dark:text-neutral-400 text-balance max-w-xl">
       <p>
-        I&apos;m a recent Computer Science graduate from UC Santa Cruz, with a
-        focus on web products and data-driven, user-focused experiences.
-        Recently, I built{" "}
+        I&apos;m a recent Computer Science graduate from UC Santa Cruz focused
+        on building user-facing products, from data to UI. Recently, I built{" "}
         <ExternalLink
           href="https://padoh.vercel.app"
           className="hover:text-primary"
         >
           Padoh
         </ExternalLink>
-        , a K-pop analytics app, led the frontend for BudgetByte, a
-        receipt-to-nutrition insights app, and taught computer science to middle
+        , led the frontend for BudgetByte, and taught computer science to middle
         and high school students.
       </p>
     </div>
